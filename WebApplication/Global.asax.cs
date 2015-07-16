c1d76315c6943702891216fe7d783f03e8027473
Anton Bevzuk <anton.bevzuk@smartstepgroup.com>
2015-07-16 17:57:17 +0300

    Minor changes

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿#region Usings
+
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+#endregion
 
 namespace WebApplication
 {
